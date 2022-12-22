1d13a5bc055f4d3a5e581dc84a0ed6d8878c647a
Reeve Fernandes <ferna312@msu.edu>
2022-12-22 14:39:37 -0800

    ExpenseDate component added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  //   const expenseDate = new Date(2021, 2, 28);
-  //   const expenseTitle = "Car Insurance";
-  //   const expenseAmount = 249.67;
+  //   const month = props.date.toLocaleString("en-US", { month: "long" });
+  //   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  //   const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
