469cfdf618b19d789173c4f52b1c37397494b18b
Reeve Fernandes <ferna312@msu.edu>
2023-05-14 19:41:17 +0400

    add submitting functionality and clearing form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -34,26 +34,41 @@ const ExpenseForm = () => {
     };
 
     const dateChangeHandler = event => {
-        setEnteredDate(event.targer.value);
+        setEnteredDate(event.target.value);
         //  setUserInput({
         //     ...userInput,
         //     enteredDate: event.target.value,
         // });
     };
 
-    return <form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData ={
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+        setEnteredAmount('');
+        setEnteredDate('');
+        setEnteredTitle('');
+
+    };
+
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}/>
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
