c14822a00681b27ad0f090d602ee7a88c224b18d
Reeve Fernandes <ferna312@msu.edu>
2023-05-17 17:55:14 +0400

    bug fix

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -21,7 +21,6 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
       <br></br>
     </Card>
   );
